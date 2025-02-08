c48f389836b1e30b4ea03e1c55675abf98bfe275
Yannkouam <yann-kevin.kouam-simo@2026.icam.fr>
2025-02-08 12:05:41 +0100

    publish (select from gallery)

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,68 +1,254 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+// 1. Page principale affichant les annonces
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Liste des annonces (sous forme de données simulées)
+  List<Map<String, dynamic>> annonces = [
+    {
+      'title': 'Sofa en cuir',
+      'price': 500.0,
+      'description': 'Sofa en cuir noir, utilisé mais en bon état.',
+      'status': 'Occasion',
+      'author': 'Jean Dupont',
+      'date': '12/01/2025',
+      'likes': 10,
+      'dislikes': 1,
+      'image': 'assets/sofa.jpg',
+    },
+    {
+      'title': 'Table en bois',
+      'price': 150.0,
+      'description': 'Table en bois massif, très bon état.',
+      'status': 'Neuf',
+      'author': 'Marie Durand',
+      'date': '14/01/2025',
+      'likes': 20,
+      'dislikes': 2,
+      'image': 'assets/table.jpg',
+    },
+  ];
+
+  // Variable pour afficher les détails d'une annonce
+  Map<String, dynamic>? selectedAnnonce;
+
+  void _likeAnnonce() {
+    setState(() {
+      selectedAnnonce!['likes'] += 1;
+    });
+  }
+
+  void _dislikeAnnonce() {
+    setState(() {
+      selectedAnnonce!['dislikes'] += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Accueil"),
-      ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          double screenWidth = constraints.maxWidth;
-          int crossAxisCount = screenWidth > 600
-              ? 3
-              : 1; // 3 colonnes sur grand écran, 1 sur mobile
-
-          return Padding(
-            padding: EdgeInsets.all(10.0),
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: crossAxisCount,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                childAspectRatio: 4 / 3,
+      appBar: selectedAnnonce == null
+          ? AppBar(
+              title: Text("Accueil"),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    // Logique de publication d'une nouvelle annonce
+                  },
+                ),
+              ],
+            )
+          : AppBar(
+              title: Text("Détail de l'annonce"),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  setState(() {
+                    selectedAnnonce = null; // Retourner à la liste des annonces
+                  });
+                },
               ),
-              itemCount: 6, // Nombre de publications simulées
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.grey[300],
+            ),
+      body: selectedAnnonce == null
+          ? Padding(
+              padding: EdgeInsets.all(10.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 4 / 3,
+                ),
+                itemCount: annonces.length,
+                itemBuilder: (context, index) {
+                  var annonce = annonces[index];
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedAnnonce = annonce;
+                      });
+                    },
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: Colors.grey[300],
+                                  image: DecorationImage(
+                                    image: AssetImage(annonce['image']),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
                             ),
-                            child: Center(child: Text("Image ${index + 1}")),
-                          ),
+                            SizedBox(height: 8),
+                            Text(
+                              annonce['title'],
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "Prix : ${annonce['price']}€",
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                          ],
                         ),
-                        SizedBox(height: 8),
-                        Text(
-                          "Titre de la publication ${index + 1}",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          : Padding(
+              padding: EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset(
+                        selectedAnnonce!['image'],
+                        width: double.infinity,
+                        height: 250,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      selectedAnnonce!['title'],
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Prix : ${selectedAnnonce!['price']}€',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.green,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      'Description :',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      selectedAnnonce!['description'],
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      'Statut : ${selectedAnnonce!['status']}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Publié par : ${selectedAnnonce!['author']}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Date de publication : ${selectedAnnonce!['date']}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.thumb_up, color: Colors.blue),
+                          onPressed: _likeAnnonce,
                         ),
-                        SizedBox(height: 4),
+                        SizedBox(width: 4),
                         Text(
-                            "Description courte de la publication ${index + 1}.",
-                            style: TextStyle(color: Colors.grey[700])),
+                          '${selectedAnnonce!['likes']} Likes',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(width: 16),
+                        IconButton(
+                          icon: Icon(Icons.thumb_down, color: Colors.red),
+                          onPressed: _dislikeAnnonce,
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          '${selectedAnnonce!['dislikes']} Dislikes',
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ],
                     ),
-                  ),
-                );
-              },
+                    SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Logique de réservation
+                      },
+                      child: Text(
+                        "Réserver",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orangeAccent,
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          );
-        },
-      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
 }
--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -1,46 +1,258 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; // Import pour choisir une image
+import 'dart:io'; // Pour utiliser File
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
-    final TextEditingController titleController = TextEditingController();
-    final TextEditingController descriptionController = TextEditingController();
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Publier une annonce'),
+        title: const Text('Mes annonces'),
+        backgroundColor: Colors.orangeAccent,
       ),
       body: Padding(
-        padding: EdgeInsets.all(screenWidth * 0.05),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
-              controller: titleController,
-              decoration: const InputDecoration(labelText: 'Titre de l\'annonce'),
+            // Bloc pour déposer une nouvelle annonce
+            Card(
+              color: Colors.orange[100],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: ListTile(
+                leading:
+                    const Icon(Icons.add_box, size: 40, color: Colors.orange),
+                title: const Text(
+                  'Créer une nouvelle annonce',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: const Text('Ajoutez votre annonce ici.'),
+                trailing:
+                    const Icon(Icons.arrow_forward_ios, color: Colors.orange),
+                onTap: () {
+                  // Logique pour aller à la page de création d'annonce
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CreatePostPage()),
+                  );
+                },
+              ),
             ),
             const SizedBox(height: 20),
-            TextField(
-              controller: descriptionController,
-              maxLines: 4,
-              decoration: const InputDecoration(labelText: 'Description'),
+
+            // Bloc pour les annonces en cours
+            const Text(
+              'Vos annonces en cours',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Logique pour publier l'annonce
-                print('Titre : ${titleController.text}');
-                print('Description : ${descriptionController.text}');
-              },
-              child: const Text('Publier'),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount:
+                    3, // Exemple avec 3 annonces, remplacez par votre liste dynamique
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: ListTile(
+                      leading: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Icon(Icons.image, color: Colors.grey),
+                      ),
+                      title: Text('Annonce ${index + 1}'),
+                      subtitle: const Text('Statut : En cours'),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.orange),
+                        onPressed: () {
+                          // Logique pour éditer l'annonce
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+// Page de création d'annonce avec sélection de catégorie et photo
+class CreatePostPage extends StatefulWidget {
+  const CreatePostPage({super.key});
+
+  @override
+  _CreatePostPageState createState() => _CreatePostPageState();
+}
+
+class _CreatePostPageState extends State<CreatePostPage> {
+  final List<String> categories = [
+    'Vêtements',
+    'Électronique',
+    'Mobilier',
+    'Sports',
+    'Jouets'
+  ];
+  String selectedCategory = 'Vêtements'; // Valeur par défaut
+  File? _image; // Variable pour stocker l'image choisie
+
+  // Méthode pour choisir une image depuis la galerie
+  Future<void> _pickImageFromGallery() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path); // Stocke le chemin de l'image
+      });
+    }
+  }
+
+  // Méthode pour prendre une photo avec la caméra
+  Future<void> _pickImageFromCamera() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path); // Stocke le chemin de l'image
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Créer une annonce'),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Champ de sélection de catégorie
+            const Text(
+              'Choisir une catégorie',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            DropdownButton<String>(
+              value: selectedCategory,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedCategory = newValue!;
+                });
+              },
+              items: categories.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 20),
+
+            // Champ pour le titre de l'annonce
+            const Text(
+              'Titre de l\'annonce',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Entrez le titre de votre annonce',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Champ pour la description de l'annonce
+            const Text(
+              'Description de l\'annonce',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Entrez la description de votre annonce',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 4,
+            ),
+            const SizedBox(height: 20),
+
+            // Boutons pour choisir ou prendre une photo
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: _pickImageFromGallery,
+                  icon: Icon(Icons.photo_library),
+                  label: Text('Galerie'),
+                ),
+                SizedBox(width: 16),
+                ElevatedButton.icon(
+                  onPressed: _pickImageFromCamera,
+                  icon: Icon(Icons.camera_alt),
+                  label: Text('Caméra'),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+
+            // Affichage de l'image choisie ou prise
+            _image != null
+                ? Image.file(
+                    _image!,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  )
+                : Container(
+                    height: 100,
+                    width: double.infinity,
+                    color: Colors.grey[300],
+                    child: Center(
+                      child: Text('Aucune image sélectionnée'),
+                    ),
+                  ),
+            SizedBox(height: 10),
+
+            // Bouton pour publier l'annonce
+            ElevatedButton(
+              onPressed: () {
+                // Logique pour publier l'annonce
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                      content: Text(
+                          'Annonce publiée dans la catégorie $selectedCategory')),
+                );
+              },
+              child: const Text('Publier l\'annonce'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 14),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: PostPage(),
+  ));
 }
